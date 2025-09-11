fafb4262b20ca98b8209101a6e242219dc6a716c
Flor Escobar <florescobar919@gmail.com>
2025-09-11 10:26:56 +0200

    Update tests2.py

--- tests2.py
+++ tests2.py
@@ -2,7 +2,6 @@ import reminder as app
 
 from reminder import Task
 
-
 def test_find_task():
     task_list = [Task(name="pay rent"), Task(name="pay bills")]
     assert app._find_task("pay bills", task_list) == Task(name="pay bills")
